2caac5987c77babbed310a2cf460d2852247f3b9
Chakradhar06-Git <k.chakradharreddy1999@gmail.com>
2024-12-20 13:09:21 +0530

    Add files via upload

---
+++ AMP/Libs/CustomKeywords.groovy
@@ -0,0 +1,10 @@
+
+/**
+ * This class is generated automatically by Katalon Studio and should not be modified or deleted.
+ */
+
+
+
+def static "AmpKeywords.AddActionCommercialESG"() {
+    (new AmpKeywords()).AddActionCommercialESG()
+}
